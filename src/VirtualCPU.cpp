1e9d4552f6f8d643af1cfd693e60b9f094d3b111
gnilk <wef@wef.com>
2023-12-14 13:42:56 +0100

    Initial commit

---
+++ src/VirtualCPU.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gnilk on 14.12.23.
+//
+
+#include "VirtualCPU.h"
---
+++ src/VirtualCPU.h
@@ -0,0 +1,17 @@
+//
+// Created by gnilk on 14.12.23.
+//
+
+#ifndef VIRTUALCPU_H
+#define VIRTUALCPU_H
+
+
+namespace gnilk {
+    class VirtualCPU {
+
+    };
+}
+
+
+
+#endif //VIRTUALCPU_H
