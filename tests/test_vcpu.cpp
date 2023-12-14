987894b5520db8ee985a89214fbaef961df7c097
gnilk <wef@wef.com>
2023-12-14 13:53:49 +0100

    Basic unit test infrastructure

--- tests/test_vcpu.cpp
+++ tests/test_vcpu.cpp
@@ -1,3 +1,16 @@
 //
 // Created by gnilk on 14.12.23.
 //
+#include <testinterface.h>
+
+extern "C" {
+    DLL_EXPORT int test_vcpu(ITesting *t);
+    DLL_EXPORT int test_vcpu_create(ITesting *t);
+}
+
+DLL_EXPORT int test_vcpu(ITesting *t) {
+    return kTR_Pass;
+}
+DLL_EXPORT int test_vcpu_create(ITesting *t) {
+    return kTR_Pass;
+}
