c85a8e2899b5a757266d57f4adb911196beb772e
gnilk <wef@wef.com>
2023-12-14 16:13:47 +0100

    NEW: More cases in test
    8,16,32,64 bits immediate moves are now tested
    d0,d1,d2,d3 registers are tested..

--- tests/test_vcpu.cpp
+++ tests/test_vcpu.cpp
@@ -27,11 +27,37 @@ DLL_EXPORT int test_vcpu_instr_move_immediate(ITesting *t) {
         0x20,0b00001101,0x44,
         0x21,0b00001101,0x44,0x33,
         0x22,0b00001101,0x44,0x33,0x22,0x11,
+        0x23,0b00001101,0x88,0x77,0x66,0x55,0x44,0x33,0x22,0x11,
+
+        0x21,0b0001'1101,0x44,0x33,
+        0x20,0b0010'1101,0x44,
+        0x21,0b0011'1101,0x44,0x33,
+        0x22,0b0100'1101,0x44,0x33,0x22,0x11,
+
     };
     VirtualCPU vcpu;
     vcpu.Begin(program, 1024);
-    vcpu.Step();
     auto &regs = vcpu.GetRegisters();
+    // Verify intermediate mode reading works for 8,16,32,64 bit sizes
+    vcpu.Step();
+    TR_ASSERT(t, regs.dataRegisters[0].data.word == 0x4433);
+    vcpu.Step();
     TR_ASSERT(t, regs.dataRegisters[0].data.byte == 0x44);
+    vcpu.Step();
+    TR_ASSERT(t, regs.dataRegisters[0].data.word == 0x4433);
+    vcpu.Step();
+    TR_ASSERT(t, regs.dataRegisters[0].data.dword == 0x44332211);
+    vcpu.Step();
+    TR_ASSERT(t, regs.dataRegisters[0].data.longword == 0x8877665544332211);
+
+    // Check if we can distribute across different registers
+    vcpu.Step();
+    TR_ASSERT(t, regs.dataRegisters[1].data.word == 0x4433);
+    vcpu.Step();
+    TR_ASSERT(t, regs.dataRegisters[2].data.byte == 0x44);
+    vcpu.Step();
+    TR_ASSERT(t, regs.dataRegisters[3].data.word == 0x4433);
+    vcpu.Step();
+    TR_ASSERT(t, regs.dataRegisters[4].data.dword == 0x44332211);
     return kTR_Pass;
 }
