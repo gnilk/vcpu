042f1cce9d65efcf1cf50f9677d2f65fddacbcf5
gnilk <wef@wef.com>
2023-12-15 09:40:03 +0100

    Unit test for add instr

--- tests/test_vcpu.cpp
+++ tests/test_vcpu.cpp
@@ -12,9 +12,13 @@ extern "C" {
     DLL_EXPORT int test_vcpu(ITesting *t);
     DLL_EXPORT int test_vcpu_instr_move_immediate(ITesting *t);
     DLL_EXPORT int test_vcpu_instr_move_reg2reg(ITesting *t);
+    DLL_EXPORT int test_vcpu_instr_add_immediate(ITesting *t);
+    DLL_EXPORT int test_vcpu_instr_add_reg2reg(ITesting *t);
 }
 
 DLL_EXPORT int test_vcpu(ITesting *t) {
+    t->CaseDepends("instr_add_immediate", "instr_move_immediate");
+    t->CaseDepends("instr_add_reg2reg", "instr_move_reg2reg");
     return kTR_Pass;
 }
 
@@ -93,6 +97,61 @@ DLL_EXPORT int test_vcpu_instr_move_reg2reg(ITesting *t) {
     TR_ASSERT(t, regs.dataRegisters[1].data.word == regs.dataRegisters[5].data.word);
     vcpu.Step();
     TR_ASSERT(t, regs.dataRegisters[2].data.byte == regs.dataRegisters[5].data.byte);
+
+    return kTR_Pass;
+}
+
+DLL_EXPORT int test_vcpu_instr_add_immediate(ITesting *t) {
+    uint8_t byteAdd[]= {
+        // add.b d0, 0x44
+        0x30,0x00,0x03,0x01, 0x44,
+    };
+    uint8_t wordAdd[]= {
+        // add.w d0, 0x4433
+        0x30,0x01,0x03,0x01, 0x44,0x33,
+    };
+    VirtualCPU vcpu;
+    auto &regs = vcpu.GetRegisters();
+    // Test 'add-value-to-zeroed-out-register'
+    regs.dataRegisters[0].data.longword = 0;
+
+    vcpu.Begin(byteAdd, 1024);
+    vcpu.Step();
+    TR_ASSERT(t, regs.dataRegisters[0].data.byte == 0x44);
+
+    vcpu.Reset();
+    vcpu.Begin(wordAdd, 1024);
+    vcpu.Step();
+    TR_ASSERT(t, regs.dataRegisters[0].data.word == 0x4433);
+
+    return kTR_Pass;
+}
+
+DLL_EXPORT int test_vcpu_instr_add_reg2reg(ITesting *t) {
+    uint8_t program[]= {
+        0x30,0x01,0x13,0x03,    // add.w d1, d0     // d1 = 0 + 0x4711
+        0x30,0x01,0x53,0x13,    // add.w d5, d1     // d5 = 0 + 0x4711
+        0x30,0x00,0x23,0x53,    // add.b d2, d5     // d2 = 0 + 0x11
+        0x30,0x00,0x23,0x01, 0x01,  // add.b d2, 0x01 // d2 = 0x12
+    };
+    VirtualCPU vcpu;
+    vcpu.Begin(program, 1024);
+    auto &regs = vcpu.GetRegisters();
+    // preload reg d0 with 0x477
+    regs.dataRegisters[0].data.word = 0x4711;
+
+    // Verify intermediate mode reading works for 8,16,32,64 bit sizes
+    vcpu.Step();
+    TR_ASSERT(t, regs.dataRegisters[1].data.word == 0x4711);
+    vcpu.Step();
+    TR_ASSERT(t, regs.dataRegisters[1].data.word == 0x4711);
+    TR_ASSERT(t, regs.dataRegisters[5].data.word == 0x4711);
+    vcpu.Step();
+    TR_ASSERT(t, regs.dataRegisters[2].data.byte == 0x11);
+    vcpu.Step();
+    TR_ASSERT(t, regs.dataRegisters[2].data.byte == 0x12);
+
+    return kTR_Pass;
 
     return kTR_Pass;
 }
