82b7b7763f4a0a24e1a3607eb37754ae34c8c1d0
gnilk <1664069+gnilk@users.noreply.github.com>
2023-12-18 16:55:18 +0100

    Unit test for array declaration

--- src/assembler/tests/test_parser.cpp
+++ src/assembler/tests/test_parser.cpp
@@ -9,6 +9,7 @@ using namespace gnilk::assembler;
 extern "C" {
     DLL_EXPORT int test_parser(ITesting *t);
     DLL_EXPORT int test_parser_instruction(ITesting *t);
+    DLL_EXPORT int test_parser_declaration(ITesting *t);
 }
 DLL_EXPORT int test_parser(ITesting *t) {
     return kTR_Pass;
@@ -17,6 +18,15 @@ DLL_EXPORT int test_parser_instruction(ITesting *t) {
     Parser parser;
 
     auto ast = parser.ProduceAST("move.l d0,0x45");
+    TR_ASSERT(t, ast != nullptr);
+    ast->Dump();
+    return kTR_Pass;
+}
+
+DLL_EXPORT int test_parser_declaration(ITesting *t) {
+    Parser parser;
+
+    auto ast = parser.ProduceAST("data: dc.b 1,2,3,4");
     TR_ASSERT(t, ast != nullptr);
     ast->Dump();
     return kTR_Pass;
