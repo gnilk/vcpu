02e55bd7ed45c37e4cb4ac17c0cdc1bb42ad13b3
gnilk <1664069+gnilk@users.noreply.github.com>
2023-12-19 14:15:10 +0100

    NEW: Added comment about merging of segments in the test-asm file

--- Assets/test.asm
+++ Assets/test.asm
@@ -10,6 +10,10 @@
     move.b  d0, (a0)
     brk
 
+;
+; this will be merged at the end anyway, if you want data items within your code-statements you should not switch
+; segments... just declare them up front..
+;
     .data
 datablock:
     dc.b    0xff,2,3,4,5,6,7
