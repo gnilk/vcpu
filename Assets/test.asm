b362dfe925d449b999c4335ab52c9689edbcad6e
gnilk <1664069+gnilk@users.noreply.github.com>
2023-12-19 15:32:27 +0100

    FIX: pseudo code in comments for next step

--- Assets/test.asm
+++ Assets/test.asm
@@ -1,10 +1,18 @@
 ;----------------------
 ; asm file
 ;----------------------
-    .org 0x100
+; struct int_table absolute 0     ;; do I want this??   I don't need it - sugar...
+;    syscall_1   rs.l    1       ;; rs = ReServe
+;    syscall_2   rs.l    1
+;    syscall_3   rs.l    1
+; endstruct
+
+    .org 0x2000
     .code
 ; BASE EQU 45
-
+;    move.l  a0, 0       ;; address of int.table
+;    lea     a0, int_table           ;; should load the absolute address of 0
+;    call    int_table.syscall_1      ;; this would work, if I can support 'absolute' need to see how the compiler handles this...
 
     lea     a0, datablock
     move.b  d0, (a0)
