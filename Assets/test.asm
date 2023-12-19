8d0878bcaad142b1adf90cee3d4349a234337154
gnilk <1664069+gnilk@users.noreply.github.com>
2023-12-19 08:32:51 +0100

    Test asset now includes meta-statements

--- Assets/test.asm
+++ Assets/test.asm
@@ -1,8 +1,17 @@
+;----------------------
+; asm file
+;----------------------
+    .org 0x100
+    .code
+; BASE EQU 45
 
 
     lea     a0, datablock
     move.b  d0, (a0)
     brk
 
+    .data
 datablock:
     dc.b    0xff,2,3,4,5,6,7
+
+    .text
