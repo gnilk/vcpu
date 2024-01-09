d898929fc8cb46774a27c99f9c70bc9660a3fa8d
gnilk <1664069+gnilk@users.noreply.github.com>
2024-01-09 15:43:29 +0100

    NEW: First try of mmu page handling

--- src/vcpu/MemoryUnit.cpp
+++ src/vcpu/MemoryUnit.cpp
@@ -7,6 +7,150 @@
 using namespace gnilk;
 using namespace gnilk::vcpu;
 
+// Initialize MMU and the page-table directory
+bool MemoryUnit::Initialize(void *physicalRam, size_t sizeInBytes) {
+    ptrPhysicalRamStart = physicalRam;
+    szPhysicalRam = sizeInBytes;
+    ptrCurrentPage = static_cast<uint8_t *>(ptrPhysicalRamStart);
+
+    // Allocate one page table for the mmu on initialization
+    rootTables = (PageTable *)physicalRam;
+    InitializeRootTables();
+
+    ptrCurrentPage += sizeof(PageTable);
+
+    return true;
+}
+
+// Internal, initialize the root tables in the current physical RAM
+void MemoryUnit::InitializeRootTables() {
+    for(int i=0;i<VCPU_MMU_MAX_ROOT_TABLES;i++) {
+        rootTables[i].nDescriptors = 0;
+        for(int idxDesc=0;idxDesc<VCPU_MMU_MAX_DESCRIPTORS;idxDesc++) {
+            rootTables[i].descriptor[idxDesc].nPages = 0;
+            for(int idxPage=0; idxPage<VCPU_MMU_MAX_PAGES_PER_DESC; idxPage++) {
+                rootTables[i].descriptor[idxDesc].pages[idxPage].ptrPhysical = nullptr;
+                rootTables[i].descriptor[idxDesc].pages[idxPage].bytes = 0;
+                rootTables[i].descriptor[idxDesc].pages[idxPage].flags = 0;
+            }
+        }
+    }
+}
+
+// Allocates a page from physical RAM
+uint64_t MemoryUnit::AllocatePage() {
+    return AllocateLargePage(VCPU_MMU_PAGE_SIZE);
+}
+
+uint64_t MemoryUnit::AllocateLargePage(size_t szPage) {
+
+    int64_t idxRootTable = FindFreeRootTable();
+    if (idxRootTable < 0) {
+        return 0;
+    }
+    int64_t idxDesc = FindFreePageDescriptor(idxRootTable);
+    if (idxDesc < 0) {
+        return 0;
+    }
+    int64_t idxPage = FindFreePage(idxRootTable, idxDesc);
+    if (idxPage < 0) {
+        return 0;
+    }
+
+    // Indicate number of pages, and allocate one..
+    rootTables[idxRootTable].descriptor[idxDesc].nPages++;
+    rootTables[idxRootTable].descriptor[idxDesc].pages[idxPage].bytes = szPage;
+    rootTables[idxRootTable].descriptor[idxDesc].pages[idxPage].ptrPhysical = AllocatePhysicalPage(szPage);
+    rootTables[idxRootTable].descriptor[idxDesc].pages[idxPage].flags = MMU_FLAGS_RWX;
+
+    // 12 = 4k per page..
+    uint64_t virtualRoot = (idxRootTable<<(12 + VCPU_MMU_DESCRIPTORS_NBITS + VCPU_MMU_PAGES_NBITS));
+    uint64_t virtualDesc = (idxDesc << (12 + VCPU_MMU_PAGES_NBITS));
+    uint64_t viurtualPage = (idxPage << 12);
+
+    uint64_t virtualAddress = virtualRoot | virtualDesc | viurtualPage;
+
+    return virtualAddress;
+}
+
+// Returns the page-table for a virtual address, or null if none
+// this is for debugging and unit-testing purposes...
+const MemoryUnit::Page *MemoryUnit::GetPageForAddress(uint64_t virtualAddress) {
+    uint64_t idxRootTable = virtualAddress >> (12 + VCPU_MMU_DESCRIPTORS_NBITS + VCPU_MMU_PAGES_NBITS);
+    uint64_t idxDesc = virtualAddress >> (12 + VCPU_MMU_PAGES_NBITS);
+    uint64_t idxPage = virtualAddress >> (12);
+
+    if (rootTables[idxRootTable].descriptor[idxDesc].pages[idxPage].flags == 0x00) {
+        return nullptr;
+    }
+
+    return &rootTables[idxRootTable].descriptor[idxDesc].pages[idxPage];
+}
+
+//
+// I probably need to optimize this quite a bit - have some reading to do in order to figure out a
+// FPGA/HW friendly way to do this...
+//
+
+// Finds a free root table...
+int64_t MemoryUnit::FindFreeRootTable() {
+    for(int i=0;i<VCPU_MMU_MAX_ROOT_TABLES;i++) {
+        // Must be -1 since we will allocate this free slot..
+        if (rootTables[i].nDescriptors < (VCPU_MMU_MAX_DESCRIPTORS-1)) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// Finds a free page-descriptor within the root table..
+int64_t MemoryUnit::FindFreePageDescriptor(uint64_t idxRootTable) {
+    for(int i=0;i<VCPU_MMU_MAX_DESCRIPTORS;i++) {
+        // Must be -1 since we will allocate this free slot..
+        if (rootTables[idxRootTable].descriptor[i].nPages < (VCPU_MMU_MAX_PAGES_PER_DESC-1)) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// Finds a free page within the descriptor table...
+int64_t MemoryUnit::FindFreePage(uint64_t idxRootTable, uint64_t idxDescriptor) {
+    for(int i=0;i<VCPU_MMU_MAX_PAGES_PER_DESC;i++) {
+        if (rootTables[idxRootTable].descriptor[idxDescriptor].pages[i].flags == 0x00) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// Checks if a specific virtual address is valid - basically checks if there is a
+bool MemoryUnit::IsAddressValid(uint64_t virtualAddress) {
+    uint64_t idxRootTable = virtualAddress >> (12 + VCPU_MMU_DESCRIPTORS_NBITS + VCPU_MMU_PAGES_NBITS);
+    uint64_t idxDesc = virtualAddress >> (12 + VCPU_MMU_PAGES_NBITS);
+    uint64_t idxPage = virtualAddress >> (12);
+
+    // Dont't think the following is needed - as we are guranteed to look within boundaries
+    // if (rootTables[idxRootTable].nDescriptors <= idxDesc) {
+    //     return false;
+    // }
+    // if (rootTables[idxRootTable].descriptor[idxDesc].nPages <= idxPage) {
+    //     return false;
+    // }
+    if (rootTables[idxRootTable].descriptor[idxDesc].pages[idxPage].flags == 0) {
+        return false;
+    }
+
+    return true;
+}
 uint64_t MemoryUnit::TranslateAddress(uint64_t virtualAddress) {
     return virtualAddress;
+}
+
+
+void *MemoryUnit::AllocatePhysicalPage(size_t szPage) {
+    auto *ptr = ptrCurrentPage;
+    ptrCurrentPage += szPhysicalRam;
+    return ptr;
+
 }
--- src/vcpu/MemoryUnit.h
+++ src/vcpu/MemoryUnit.h
@@ -6,14 +6,83 @@
 #define MEMORYUNIT_H
 
 #include <stdint.h>
+#include <unordered_map>
 
 namespace gnilk {
     namespace vcpu {
+        // DO NOT CHANGE
+        static const size_t VCPU_MMU_MAX_ROOT_TABLES = 16;
+        static const size_t VCPU_MMU_ROOT_TABLES_NBITS = 4;        // Should be the bit position of 16
+
+        static const size_t VCPU_MMU_MAX_DESCRIPTORS = 16;
+        static const size_t VCPU_MMU_DESCRIPTORS_NBITS = 4;
+
+        static const size_t VCPU_MMU_MAX_PAGES_PER_DESC = 16;
+        static const size_t VCPU_MMU_PAGES_NBITS = 4;
+
+        static const size_t VCPU_MMU_PAGE_SIZE = 4096;
+
+        // Page table flags...
+        static const uint32_t MMU_FLAG_READ =  0x01;    // allow read
+        static const uint32_t MMU_FLAG_WRITE = 0x02;    // allow write
+        static const uint32_t MMU_FLAG_EXEC =  0x04;    // allow execute
+
+        static const uint32_t MMU_FLAGS_RWX =  0x07;    // allow Read|Write|Exec - this is the default..
+
+
+        // addresses are always 64bit, a mapped pages is looked up like:
+        //
+        // [8][8][8][....][12] =>
+        //  8 bit table-index
+        //  8 bit descriptor-index
+        //  8 bit page-index
+        //  [12 bits unused]
+        //  12 bit page-offset
+
         class MemoryUnit {
+        public:
+#pragma pack(push,1)
+            // 32 bytes
+            struct Page {
+                void *ptrPhysical = nullptr;    // 64 bit?
+                uint32_t bytes = 0;             // 32 bit, page size - max 4gb continous memory
+                uint32_t flags = 0;  // TBD     // 32 bit
+            };
+#pragma pack(pop)
+
+            struct PageDescriptor {
+                size_t nPages = 0;
+                Page pages[VCPU_MMU_MAX_PAGES_PER_DESC];
+            };
+            struct PageTable {
+                size_t nDescriptors = 0;
+                PageDescriptor descriptor[VCPU_MMU_MAX_DESCRIPTORS];
+            };
+
         public:
             MemoryUnit() = default;
             virtual ~MemoryUnit() = default;
+            bool Initialize(void *physicalRam, size_t sizeInBytes);
+            bool IsAddressValid(uint64_t virtualAddress);
             uint64_t TranslateAddress(uint64_t virtualAddress);
+            uint64_t AllocatePage();
+            uint64_t AllocateLargePage(size_t szPage);
+            const Page *GetPageForAddress(uint64_t virtualAddress);
+        protected:
+            void *AllocatePhysicalPage(size_t szPage);
+            void InitializeRootTables();
+
+            int64_t FindFreeRootTable();
+            int64_t FindFreePageDescriptor(uint64_t idxRootTable);
+            int64_t FindFreePage(uint64_t idxRootTable, uint64_t idxDescriptor);
+        private:
+            void *ptrPhysicalRamStart = nullptr;
+            size_t szPhysicalRam = 0;
+
+            uint8_t *ptrCurrentPage = nullptr;
+            size_t idxCurrentPhysicalPage = 0;
+
+            PageTable *rootTables;
         };
     }
 }
