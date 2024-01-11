ed9f958dd41d79afc40453d2274b34062aeb9d27
gnilk <1664069+gnilk@users.noreply.github.com>
2024-01-11 13:11:00 +0100

    FIX: Segment declaration was bogus in source...

--- Assets/hello.asm
+++ Assets/hello.asm
@@ -2,13 +2,12 @@
 // Created by gnilk on 11.01.2024.
 //
     .code
-
-    .data
 _start:
     lea     a0, string
     move.l  d0, 0x01
     syscall
     ret
 
+    .data
 string:
     dc.b    "hello world",0      ;; you need to terminate the strings!!!
