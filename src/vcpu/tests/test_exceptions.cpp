837272f5aca6e87dee2c825a0f051a0334a6de6e
gnilk <1664069+gnilk@users.noreply.github.com>
2024-05-13 14:30:04 +0200

    FIX: Added test for nested exceptions
    This test has an invalid instr. in the exception code block which will cause the exception handler to be raised.

--- src/vcpu/tests/test_exceptions.cpp
+++ src/vcpu/tests/test_exceptions.cpp
@@ -22,6 +22,7 @@ extern "C" {
     DLL_EXPORT int test_exceptions(ITesting *t);
     DLL_EXPORT int test_exceptions_illegal_instr(ITesting *t);
     DLL_EXPORT int test_exceptions_in_isr(ITesting *t);
+    DLL_EXPORT int test_exceptions_nested(ITesting *t);
 }
 DLL_EXPORT int test_exceptions(ITesting *t) {
     return kTR_Pass;
@@ -152,8 +153,67 @@ DLL_EXPORT int test_exceptions_in_isr(ITesting *t) {
     }
 
     fmt::println("irq counter={}", irq_counter);
-    fmt::println("irq counter={}", exp_counter);
+    fmt::println("exp counter={}", exp_counter);
+
+    TR_ASSERT(t, irq_counter > 0);
+    TR_ASSERT(t, exp_counter == 1);
 
     return kTR_Pass;
 }
 
+DLL_EXPORT int test_exceptions_nested(ITesting *t) {
+    VirtualCPU vcpu;
+    // See: 'Interrupt.h' for definition
+    ISR_VECTOR_TABLE isrTable = {
+            .exp_illegal_instr = 0x1000,
+    };
+
+    uint8_t expRoutine[]={
+            // move.l d0,0x01
+            0x20,0x03,0x03,0x01, 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x01,
+            // syscall
+            OperandCode::SYS,
+            // nop
+            OperandCode::NOP,
+            // Illegal
+            0x01,
+            // rti
+            OperandCode::RTE,
+    };
+    uint8_t mainCode[]={
+            // nop,nop,nop,brk
+            0x01,   // This OP-Code is not defined - we should probably have a specific to raise the exception
+            OperandCode::NOP, OperandCode::NOP, OperandCode::NOP, OperandCode::BRK
+    };
+    vcpu.Begin(ram, 32*4096);
+    vcpu.LoadDataToRam(0, &isrTable, sizeof(isrTable));
+    vcpu.LoadDataToRam(0x1000, expRoutine, sizeof(expRoutine));
+    vcpu.LoadDataToRam(0x2000, mainCode, sizeof(mainCode));
+
+    int exp_counter = 0;
+    vcpu.RegisterSysCall(0x01, "count",[&exp_counter](Registers &regs, CPUBase *cpu) {
+        exp_counter++;
+        fmt::println("'count' called from exception handler, counter={}",exp_counter);
+    });
+
+    vcpu.SetInstrPtr(0x2000);
+    vcpu.EnableException(CPUExceptionFlag::InvalidInstruction);
+    for(int i=0;i<15;i++) {
+        vcpu.Step();
+        // this print is quite wrong..
+        if ((vcpu.GetISRState() == CPUISRState::Executing) || (!vcpu.GetStatusReg().flags.halt)) {
+            fmt::println("{} {}{:#x} {}",i,
+                         (vcpu.GetLastDecodedInstr()->GetISRStateBefore()==CPUISRState::Executing)?'*':' ',
+                         vcpu.GetLastDecodedInstr()->cpuRegistersBefore.instrPointer.data.longword,
+                         vcpu.GetLastDecodedInstr()->ToString());
+        } else {
+            fmt::println("{} --- halted ---", i);
+        }
+        std::this_thread::sleep_for(std::chrono::microseconds(250));
+    }
+
+    fmt::println("exp counter={}", exp_counter);
+    TR_ASSERT(t, exp_counter == 1);
+
+    return kTR_Pass;
+}
