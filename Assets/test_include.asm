957066f7a617048ac184fc19d9f607f0aae0d01f
gnilk <1664069+gnilk@users.noreply.github.com>
2025-02-04 17:18:28 +0100

    FIX: src file to test include handling

--- Assets/test_include.asm
+++ Assets/test_include.asm
@@ -1,7 +1,10 @@
 
-    .include "src_to_include.inc"
+    .include "Assets/src_to_include.inc"
+    # I used this to test include path settings, but they don't propagate yet when making so commenting it out
+    # .include "sys/dummy.inc"
 
     .code
 ; this should work
     move.l d0, A_CONSTANT
+   # move.l d1, B_CONSTANT
     ret
